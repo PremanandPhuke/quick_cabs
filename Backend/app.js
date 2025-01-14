4b0b78710aa9f859b37c05a9ce50a148d6a81058
PremanandPhuke <premanandphuke2002@gmail.com>
2025-01-14 19:22:46 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,25 @@
+// We use dotenv for running projects on different ports,
+// whereas which port we need to use will be decided by using dynamic variables 
+// and to configure that port by our project that's why we are using "dotenv"
+const dotenv= require('dotenv');
+dotenv.config();
+
+// we use cors for accepring request from a specified websites or specified links 
+// and after deploying we will put our project link in cors(_link_) so it will accept req 
+// only from that link or website
+const cors = require('cors');
+const express= require('express');
+const connectToDB = require('./db/db');
+
+const app= express();
+
+
+app.use(cors());
+connectToDB();
+
+app.get('/', (seq, res)=>{
+    console.log("Backend Running");
+    res.send("Hello World")
+});
+
+module.exports= app;
